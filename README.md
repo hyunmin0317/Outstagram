32a29213e21b656aff3eea117692c4a4ac63d68a
hyunmin0317 <choihm9903@naver.com>
2021-07-12 18:55:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Java Programming
+
+### 자바 프로그래밍 (2학년 2학기)
+
+---
+
+### chapter 01 자바 시작
+
+### chapter 02 자바 기본 프로그래밍
+
+### chapter 03 반복문과 배열 그리고 예외처리
+
+### chapter 04 클래스와 객체
+
+### chapter 05 상속
+
+### chapter 06 모듈과 패키지
+
+### chapter 07 제네릭과 컬렉션
+
+### chapter 08 입출력 스트림과 파일 입출력
+
---
+++ app/src/main/java/com/example/outstagram/EmailSignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.outstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_signup);
+    }
+}
