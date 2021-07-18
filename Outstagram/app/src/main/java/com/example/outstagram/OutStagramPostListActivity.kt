1cd72ee41f9a14de29ddbf2303dcd66080d83989
hyunmin0317 <choihm9903@naver.com>
2021-07-18 13:18:52 +0900

    Update Outstagram

--- Outstagram/app/src/main/java/com/example/outstagram/OutStagramPostListActivity.kt
+++ Outstagram/app/src/main/java/com/example/outstagram/OutStagramPostListActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -45,6 +46,7 @@ class OutStagramPostListActivity : AppCompatActivity() {
                             LayoutInflater.from(this@OutStagramPostListActivity),
                             glide
                         )
+                        postList.reverse()
                         post_recyclerview.adapter = adapter
                         post_recyclerview.layoutManager = LinearLayoutManager(this@OutStagramPostListActivity)
                     }
