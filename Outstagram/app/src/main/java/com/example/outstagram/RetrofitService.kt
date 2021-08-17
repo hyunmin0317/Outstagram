e7ba130c0f80503d4cd770e41124c493154e2cfb
hyunmin0317 <choihm9903@naver.com>
2021-08-17 23:24:29 +0900

    Delete

--- Outstagram/app/src/main/java/com/example/outstagram/RetrofitService.kt
+++ Outstagram/app/src/main/java/com/example/outstagram/RetrofitService.kt
@@ -8,19 +8,6 @@ import retrofit2.http.*
 
 interface RetrofitService {
 
-    @GET("json/students/")
-    fun getStudentsList(): Call<ArrayList<PersonFromServer>>
-
-    @POST("json/students/")
-    fun createStudnet(
-        @Body params: HashMap<String, Any>
-    ): Call<PersonFromServer>
-
-    @POST("json/students/")
-    fun createStudentEasy(
-        @Body person: PersonFromServer
-    ): Call<PersonFromServer>
-
     @POST("user/signup/")
     @FormUrlEncoded
     fun register(
