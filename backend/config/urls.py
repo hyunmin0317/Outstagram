8128b6769962e6cc5d3832b656d89e7033f3dc04
hyunmin0317 <choihm9903@naver.com>
2021-08-22 14:26:43 +0900

    Delete backend

--- backend/config/urls.py
+++
@@ -1,9 +0,0 @@
-# mysite/urls.py
-from django.urls import path, include
-from django.contrib import admin
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("user/", include("api.urls")),
-    path("api/auth", include("knox.urls")),
-]
---
+++ frontend/app/src/main/java/com/example/outstagram/User.kt
@@ -0,0 +1,8 @@
+package com.example.outstagram
+
+import java.io.Serializable
+
+class User(
+    var username : String? = null,
+    var token : String? = null
+):Serializable
