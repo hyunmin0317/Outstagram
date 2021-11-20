f029a6cccffde66d987b107adb62d815f85e979d
hyunmin0317 <choihm9903@naver.com>
2021-11-20 19:13:26 +0900

    Update Profile

--- frontend/app/src/main/java/com/example/outstagram/UserInfo.kt
+++ frontend/app/src/main/java/com/example/outstagram/UserInfo.kt
@@ -35,7 +35,7 @@ class UserInfo : AppCompatActivity() {
                     else
                         setImage(profile!!.image)
                 } else {
-                    Toast.makeText(this@UserInfo, "400 Bad Request", Toast.LENGTH_LONG).show()
+                    setImage("https://github.com/hyunmin0317/Outstagram/blob/master/github/basic.jpg?raw=true")
                 }
             }
 
@@ -52,16 +52,18 @@ class UserInfo : AppCompatActivity() {
         profile_delete.setOnClickListener {
             (application as MasterApplication).service.deleteProfile(username).enqueue(object :
                 Callback<Profile> {
-                override fun onResponse(call: Call<Profile>, response: Response<Profile>) {}
+                override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                    (application as MasterApplication).service.uploadProfile(username).enqueue(object :
+                        Callback<Profile> {
+                        override fun onResponse(call: Call<Profile>, response: Response<Profile>) {}
+
+                        override fun onFailure(call: Call<Profile>, t: Throwable) {}
+                    })
+                }
 
                 override fun onFailure(call: Call<Profile>, t: Throwable) {}
             })
-            (application as MasterApplication).service.uploadProfile(username).enqueue(object :
-                Callback<Profile> {
-                override fun onResponse(call: Call<Profile>, response: Response<Profile>) {}
 
-                override fun onFailure(call: Call<Profile>, t: Throwable) {}
-            })
             startActivity(Intent(this, UserInfo::class.java))
         }
 
